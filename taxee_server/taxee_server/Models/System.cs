101879a437a99e3e22691e07387832a442ac2cb4
Stephen Odundo <steveremdi@gmail.com>
2021-10-26 22:47:53 +0300

    Added model for star systems

---
+++ taxee_server/taxee_server/Models/System.cs
@@ -0,0 +1,26 @@
+﻿namespace taxee_server.Models
+{
+    public partial class System
+    {
+        public string name { get; set; }
+        public Coordinates coords { get; set; }
+        public Body[] bodies { get; set; }
+        public Station[] Stations { get; set; }
+        public class Coordinates
+        {
+            public float x { get; set; }
+            public float y { get; set; }
+            public float z { get; set; }
+        }
+        public class Body
+        {
+            public string BodyID { get; set; }
+            public string BodyName { get; set; }
+        }
+        public class Station
+        {
+            public string StationID { get; set; }
+            public string StationName { get; set; }
+        }
+    }
+}
