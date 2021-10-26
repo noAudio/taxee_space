5ab28d876ed0899f82bfb94c48622e5f96c9e7be
Stephen Odundo <steveremdi@gmail.com>
2021-10-26 23:10:53 +0300

    Added base api logic for searches

---
+++ taxee_server/taxee_server/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using taxee_server.Models;
+
+namespace taxee_server.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IEnumerable<SystemSearch> Single(string systemName)
+        {
+            throw new NotImplementedException(); //TODO Add search logic for single systems
+        }
+
+        [HttpGet]
+        public IEnumerable<SystemSearch> LiveSearch(string partialName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
