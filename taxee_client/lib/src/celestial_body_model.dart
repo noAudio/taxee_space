25c6da7a94eac1e5fee4941a27554961575020ce
Samuel Odongo <samieodd@gmail.com>
2021-10-31 05:56:18 +0300

    Added Celestial Body model

---
+++ taxee_client/lib/src/celestial_body_model.dart
@@ -0,0 +1,11 @@
+import 'dart:html';
+
+class CelestialBodyModel {
+  String name;
+  num xCoordinate;
+  num yCoordinate;
+  num zCoordinate;
+
+  CelestialBodyModel(
+      this.name, this.xCoordinate, this.yCoordinate, this.zCoordinate);
+}
