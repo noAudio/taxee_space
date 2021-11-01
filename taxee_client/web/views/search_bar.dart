420dbf52eb83d436f14d087f45c83b9ace1cbd12
Samuel Odongo <samieodd@gmail.com>
2021-11-01 23:28:41 +0300

    Removed test data

--- taxee_client/web/views/search_bar.dart
+++ taxee_client/web/views/search_bar.dart
@@ -30,60 +30,9 @@ class SearchBar {
   }
 
   void populateResults(Event event) {
-    var data = [
-      System(
-        systemID: '1471',
-        name: 'LHS 450',
-        distance: 14.78,
-        coordinates: ReceivedCoordinates(
-          x: -12.40625,
-          y: 7.8125,
-          z: -1.875,
-        ),
-      ),
-      System(
-        systemID: '25873',
-        name: 'WISE 1741+2553',
-        distance: 17.98,
-        coordinates: ReceivedCoordinates(
-          x: -12.40625,
-          y: 7.90625,
-          z: 10.34375,
-        ),
-      ),
-      System(
-        systemID: '8228',
-        name: 'Altair',
-        distance: 16.74,
-        coordinates: ReceivedCoordinates(
-          x: -12.3125,
-          y: -2.75,
-          z: 11,
-        ),
-      ),
-      System(
-        systemID: '11158',
-        name: '61 Cygni',
-        distance: 11.37,
-        coordinates: ReceivedCoordinates(
-          x: -11.21875,
-          y: -1.1875,
-          z: 1.40625,
-        ),
-      ),
-      System(
-        systemID: '9935',
-        name: 'LHS 2459',
-        distance: 17.46,
-        coordinates: ReceivedCoordinates(
-          x: -11,
-          y: 10.78125,
-          z: -8.21875,
-        ),
-      ),
-    ];
-
     var matchingSystems = <Element>[];
+
+    var data = [];
 
     for (System system in data) {
       matchingSystems.add(
