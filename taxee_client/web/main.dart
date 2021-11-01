899b6b9019f73e69d52183ce4b23f935307fb02b
Samuel Odongo <samieodd@gmail.com>
2021-11-01 03:00:06 +0300

    Added listeners for clickable elements

--- taxee_client/web/main.dart
+++ taxee_client/web/main.dart
@@ -1,1 +1,11 @@
-void main() {}
+import "dart:html";
+import 'views/search_bar.dart';
+
+void main() {
+  (querySelector('#expand-search') as Element)
+      .onClick
+      .listen(SearchBar().expandSearch);
+  (querySelector('#exchange-search') as Element)
+      .onClick
+      .listen(SearchBar().switchSearchTerms);
+}
